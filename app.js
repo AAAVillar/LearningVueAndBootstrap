611e7fc6f18ef24b3a6752f88686bd228b86e02a
Arnold Villar <furthereducation77@gmail.com>
2019-07-06 14:53:44 -0400

    I start everything all over....and this time I added bootstrap, lesson#3 net ninja Vue.js

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 
 new Vue({
+
+el:'#vue-app',  // the id is the target... of the new Vue
+
 	data:{
-		name:"bobby";
-	}
+		
+		name:"bobby",
+	    job:"Ninja-HairDresser"
+	},
+
+	methods:{
+
+	    greet:function(time){
+	    return 'Good ' + time;
+	                    },
+
+	    greet2:function(time){
+	    	return 'Good ' + time + ' ' + this.name;
+	        } // methods
+	    }
+
+
 });
