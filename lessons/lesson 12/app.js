2a24e6566d1725ec89c21a4de618d8b14637a96a
Arnold Villar <furthereducation77@gmail.com>
2019-07-25 03:20:22 -0400

    lesson 12 v-for working with for loops

--- lessons/lesson 12/app.js
+++ lessons/lesson 12/app.js
@@ -5,7 +5,13 @@ el:'#vue-app',  // the id is the target... of the new Vue
 
 	data:{
 		
-		
+		name:'jack',
+		characters:["Mario","luigi","yoshi","bowser"],
+		ninjas:[
+				{name:'Ryu',age:25},
+				{name:'Yoshi',age:35},
+				{name:'Ken',age:55}
+		       ]
 		
 	},
 
