08f8ab56f7b7c1d8521c8e12964ffdaf57a2164d
Arnold Villar <furthereducation77@gmail.com>
2019-07-26 01:43:09 -0400

    lesson 13 putting it all together

---
+++ lessons/lesson 13/app.js
@@ -0,0 +1,36 @@
+
+new Vue({
+
+el:'#vue-app',  // the id is the target... of the new Vue
+
+data:{
+	
+	health:100,
+	ended:false
+},
+
+methods:{
+
+	punch:function(){
+		this.health-=10;
+		if (this.health <= 0){
+			this.ended = true; 
+		}
+	},
+
+	restart:function(){
+		this.health = 100;
+		this.ended = false;
+	}	  
+},
+
+computed:{
+
+	
+	
+	
+	
+
+		}  // end of computed
+
+	});
