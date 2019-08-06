013ad8945327678bcb7e144de1e1ec9a585ece1e
Arnold Villar <furthereducation77@gmail.com>
2019-08-06 04:07:21 -0400

    I started using the command line with vue.js lesson 17-18

---
+++ lessons/lesson_17/vuejs-playlist/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data () {
+    return {
+      title:'Your first Vue file, how\'s that? '
+    }
+  },
+  methods:{
+    greeting:function(){
+      return 'What\'s up Doc?'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
